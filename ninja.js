352a2d761cecef36aa89f957d3b4e94b8603ccb1
Ruben Amarilla <115105166+danielamarilla@users.noreply.github.com>
2023-01-06 17:37:37 -0300

    [TAREA] sensei
    Ejercicio core

--- ninja.js
+++ ninja.js
@@ -22,3 +22,20 @@ const ninja1 = new Ninja("Hyabusa");
 ninja1.drinkSake();
 ninja1.sayName();
 ninja1.showStats();
+
+class Sensei extends Ninja {
+    constructor(name) {
+        super(name);
+        this.sabiduria = 10;
+        this.salud = 200;
+        this.velocidad = 10;
+        this.fuerza = 10;
+    }
+    speakWisdom() {
+        const masSalud = super.drinkSake();
+        console.log('"Lo que un programador puede hacer en un mes, dos programadores pueden hacerlo en dos meses."');
+    }
+}
+const superSensei = new Sensei("Master Splinter");
+superSensei.speakWisdom();
+superSensei.showStats();
